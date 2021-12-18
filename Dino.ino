ed3e8322ccfffe7f069459d4ef2db80474f25687
froggie3434 <96347184+froggie3434@users.noreply.github.com>
2021-12-18 11:31:11 -0500

    Add files via upload

--- Dino.ino
+++ Dino.ino
@@ -64,9 +64,7 @@ void setup() {
   pinMode(btnBrightness, INPUT_PULLUP);
   attachInterrupt(btnBrightness, brightness_ISR, FALLING);
   tft.init();
-  #ifdef TFT_ROTATE // I Set this variable in the TFT_eSPI User Setup   (Setup136_LilyGo_TTV requires a screen rotation)
-    tft.setRotation(TFT_ROTATE);
-  #endif
+  tft.setRotation(1);
   tft.setSwapBytes(true);
   tft.fillScreen(TFT_WHITE);
   ledcSetup(pwmLedChannelTFT, pwmFreq, pwmResolution);
